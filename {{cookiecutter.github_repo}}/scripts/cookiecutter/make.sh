6ced47fc9c1dfd8ea9ebb7d636e0a0dc79cb64e0
Joseph Burling <joseph@datajoint.com>
2022-03-08 10:44:28 -0600

    refactor(template): update regen script

--- {{cookiecutter.github_repo}}/scripts/cookiecutter/make.sh
+++ {{cookiecutter.github_repo}}/scripts/cookiecutter/make.sh
@@ -1,24 +1,97 @@
 #!/bin/bash --login
 
-# ./make.sh flit https://github.com/iamamutt/dj-cookiecutter.git cookies
+script_cmd="${BASH_SOURCE[0]}"
+script_pdir="$(cd "$(dirname "${script_cmd}")" &>/dev/null && pwd)"
+script_file=$(basename "${script_cmd}")
 
-script_pdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+git_branch=flit
+repo_url=wf
+conda_env=cookies
+build_dir="${script_pdir/build/}"
+delete_existing=false
 
-git_branch="${1:-flit}"
-repo_url="${2:-wf}"
-conda activate "${3:-cookies}"
+SHOW_HELP=0
+show_help() {
+	echo "usage: $script_file [OPTION]... build_dir
 
-if [[ -d "${script_pdir/build/}" ]]; then
-	echo "Directory 'build' already exists. Replacing existing content."
-	rm -rf "${script_pdir/build/}"
+Regenerate cookiecutter content
+
+Options:
+
+-h, --help, help ... Show this help then exit.
+
+-r .... Repository url.
+          Default=https://github.com/datajoint-company/dj-cookiecutter.git
+-b .... Branch to checkout from repository url. Set to '-' for no branch selection.
+          Default='flit'
+-n .... Conda environment name.
+          Default='cookies'
+-d .... Delete existing content on rebuild.
+          Default=false
+
+Examples:
+
+ Regenerate to directory 'build' using previously specified values.
+   > $script_file build
+
+ Regenerate to current directory using a different repo and branch, also conda env.
+   > $script_file -b flit -r https://github.com/iamamutt/dj-cookiecutter.git -n cookies .
+
+ Use local cloned content to build
+   > $script_file -b - -r  path/to/template ./build
+"
+	exit 0
+}
+
+while [[ $# -gt 0 ]]; do
+	case "$1" in
+	"help" | "--help" | "-h")
+		SHOW_HELP=1
+		break
+		;;
+	"-b")
+		git_branch="${2}"
+		shift
+		shift
+		;;
+	"-r")
+		repo_url="${2}"
+		shift
+		shift
+		;;
+	"-n")
+		conda_env="${2}"
+		shift
+		shift
+		;;
+	"-d")
+		delete_existing=true
+		shift
+		shift
+		;;
+	*)
+		build_dir="${1}"
+		shift
+		;;
+	esac
+done
+
+# show help if asked
+# ------------------
+
+[[ ${SHOW_HELP} -eq 1 ]] && show_help
+
+conda activate "${conda_env}"
+
+if [[ -d "${build_dir}" ]]; then
+	echo "Directory 'build' already exists. Overwriting existing content."
+	[[ ${delete_existing} = true ]] && rm -rf "${build_dir}"
 fi
 
-(
-	cd "${script_pdir}" || exit 1
-	cookiecutter \
-		--config-file cookiecutterc.yml \
-		--debug-file cookiecutter.log \
-		--overwrite-if-exists --no-input --output-dir build \
-		--checkout "${git_branch}"
-	${repo_url}
-)
+[[ ${git_branch} = "-" ]] && git_checkout="" || git_checkout="--checkout ${git_branch}"
+
+cookiecutter \
+	--config-file "${script_pdir/cookiecutterc.yml/}" \
+	--debug-file "${script_pdir/cookiecutter.log/}" \
+	--overwrite-if-exists --no-input --output-dir "${build_dir}" \
+	"${git_checkout}" "${repo_url}"
