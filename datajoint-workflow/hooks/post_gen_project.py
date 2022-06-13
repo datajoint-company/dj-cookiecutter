9ed3746de5c898245ebbb6dcf6f54c0f5c2cbb4d
Joseph Burling <joseph@datajoint.com>
2022-06-13 15:52:14 -0500

    refactor(workflow): refactor pyproject.toml dependencies for docker pip installs

--- datajoint-workflow/hooks/post_gen_project.py
+++ datajoint-workflow/hooks/post_gen_project.py
@@ -24,6 +24,41 @@ def delete_version_files():
         os.remove("src/{{cookiecutter.__pkg_import_name}}/version.py")
 
 
+def load_pyproject_toml(toml_file="pyproject.toml"):
+    try:
+        import tomli
+    except ModuleNotFoundError as err:
+        print(
+            f"Need 'tomli' package for loading and writing TOML files '{toml_file}'",
+            "\n>",
+            err,
+        )
+        return None
+    with open(toml_file, "rb") as tml:
+        return tomli.load(tml)
+
+
+def load_environment_yml(yaml_file="environment.yml"):
+    try:
+        import yaml
+    except ModuleNotFoundError as err:
+        print(f"Need 'pyyaml' package for loading '{yaml_file}'", "\n>", err)
+        return None
+    with open(yaml_file, "r") as yml:
+        return yaml.safe_load(yml)
+
+
+def split_deps(deps, to_move=None):
+    if isinstance(to_move, str):
+        to_move = [to_move]
+    if not to_move:
+        return deps, set()
+    regex = re.compile("|".join([f"^{req}" for req in to_move]))
+    to_pip = {req for req in deps if regex.match(req)}
+    to_conda = set(deps) - to_pip
+    return to_conda, to_pip
+
+
 def make_cookiecutterc_yml():
     json_object = read_dot_cc_json()
     if not json_object:
@@ -70,64 +105,64 @@ def update_dot_cookiecutter_json(remove_keys=None):
         jc_file.write("\n")
 
 
-def make_devcontainer_req():
-    try:
-        import tomli
-
-    except ModuleNotFoundError as err:
-        print("Need 'tomli' package for creating conda's environment.yml", "\n>", err)
+def make_devcontainer_req(requires_pip=None):
+    pyproject = load_pyproject_toml()
+    if not pyproject:
         return
 
-    with open("pyproject.toml", "rb") as tml:
-        pyproject = tomli.load(tml)
-
-    main_deps = pyproject["project"]["dependencies"]
-    main_deps = "\n".join(list(main_deps))
-    sciops_deps = pyproject["project"]["optional-dependencies"]["sciops"]
-    sciops_deps = "\n".join(list(sciops_deps))
+    conda_deps, pip_deps = split_deps(
+        pyproject["project"]["dependencies"], requires_pip
+    )
+    pip_deps = {*pip_deps, *pyproject["project"]["optional-dependencies"]["sciops"]}
 
     with open(
-        "docker/{{cookiecutter.__pkg_import_name}}_local/.devcontainer/build/requirements/conda_requirements.txt",
+        "docker/{{cookiecutter.__pkg_import_name}}_local/.devcontainer/build/"
+        "requirements/conda_requirements.txt",
         "a",
     ) as txt:
-        txt.write(main_deps)
+        txt.write("\n".join(sorted(conda_deps)) + "\n")
 
     with open(
-        "docker/{{cookiecutter.__pkg_import_name}}_local/.devcontainer/build/requirements/pip_requirements.txt",
+        "docker/{{cookiecutter.__pkg_import_name}}_local/.devcontainer/build/"
+        "requirements/pip_requirements.txt",
         "a",
     ) as txt:
-        txt.write(sciops_deps)
+        txt.write("\n".join(sorted(pip_deps)) + "\n")
 
 
-def make_conda_env_yml():
-    try:
-        import tomli
-
-    except ModuleNotFoundError as err:
-        print("Need 'tomli' package for creating conda's environment.yml", "\n>", err)
+def make_conda_env_yml(requires_pip=None):
+    pyproject = load_pyproject_toml()
+    conda_env = load_environment_yml()
+    if not pyproject or not conda_env:
         return
 
-    with open("pyproject.toml", "rb") as tml:
-        pyproject = tomli.load(tml)
+    conda_deps, pip_deps = split_deps(
+        pyproject["project"]["dependencies"], requires_pip
+    )
+    pip_deps = {*pip_deps, *pyproject["project"]["optional-dependencies"]["sciops"]}
 
-    main_deps = pyproject["project"]["dependencies"]
-    main_deps = "\n".join([f"  - {dep}" for dep in main_deps])
-    sciops_deps = pyproject["project"]["optional-dependencies"]["sciops"]
-    sciops_deps = "\n".join([f"      - {dep}" for dep in sciops_deps])
+    conda_env_dep = [i for i in conda_env.get("dependencies", []) if isinstance(i, str)]
+    conda_env_pip = [
+        i.get("pip", [])
+        for i in conda_env.get("dependencies", [])
+        if isinstance(i, dict) and "pip" in i
+    ]
+    conda_env_pip = conda_env_pip[0] if conda_env_pip else []
+    conda_env_dep.extend(conda_deps)
+    conda_env_dep = sorted(set(conda_env_dep))
+    conda_env_pip.extend(pip_deps)
+    conda_env_dep.append({"pip": sorted(set(conda_env_pip))})
+    conda_env["dependencies"] = conda_env_dep
 
-    with open("environment.yml", "a") as env:
-        env.write(
-            "  # Added from 'pyproject.toml' > " f"'.project.dependencies'\n{main_deps}"
-        )
-        env.write(
-            "\n  # Added from 'pyproject.toml' > "
-            "'.project.optional-dependencies.sciops'"
-            f"\n  - pip:\n{sciops_deps}\n"
-        )
+    import yaml
+
+    with open("environment.yml", "w") as yml:
+        yaml.dump(conda_env, yml, indent=2)
 
 
 if __name__ == "__main__":
     delete_version_files()
     update_dot_cookiecutter_json()
-    make_conda_env_yml()
-    make_devcontainer_req()
+    requires_pip = ["datajoint", "datajoint-utilities", "djsciops"]
+    make_conda_env_yml(requires_pip)
+    make_devcontainer_req(requires_pip)
